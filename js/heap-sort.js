6b80e6b621809e5024188de3bab81c3c499461e0
pavel <nechapay@list.ru>
2021-09-27 10:36:16 +0300

    heap-sort

--- js/heap-sort.js
+++ js/heap-sort.js
@@ -1,3 +1,39 @@
 const heapSort = (arr) => {
+  let n = arr.length
+  buildHeap(arr, n)
+  for (let i = n - 1; i >= 0; i--) {
+    let tmp = arr[0]
+    arr[0] = arr[i]
+    arr[i] = tmp
+    heapify(arr, i, 0)
+  }
 
+  return arr
+}
+
+function heapify(arr, n, i) {
+  let max = i
+  let left = (2 * i) + 1
+  let right = (2 * i) + 2
+
+  if(left < n && arr[left] > arr[max]) {
+    max = left
+  }
+
+  if(right < n && arr[right] > arr[max]) {
+    max = right
+  }
+
+  if (max !== i) {
+    let tmp = arr[i]
+    arr[i] = arr[max]
+    arr[max] = tmp
+    heapify(arr, n, max)
+  }
+}
+
+function buildHeap(arr, n) {
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, n, i)
+  }
 }
--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ window.addEventListener('load', function () {
   start()
 })
 function start () {
-  let arr = getArrOfRand(50)
+  let arr = getArrOfRand(6)
   myConsole(`quick sort: ${quickSort(arr, 0, arr.length - 1 )}`)
+  arr = getArrOfRand(6)
   myConsole(`merge sort: ${merge(arr)}`)
+  arr = getArrOfRand(6)
+  myConsole(`heap sort: ${heapSort(arr)}`)
 }
 
 
