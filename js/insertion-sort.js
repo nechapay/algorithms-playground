78606804bcf54170a191c83df7c8e671617abc76
nechapay <nechapay@list.ru>
2022-02-21 12:28:30 +0300

    insertion sort

---
+++ js/insertion-sort.js
@@ -0,0 +1,14 @@
+const insertionSort = (initialArray) => {
+  let arr = [...initialArray]
+  const n = arr.length
+  for (let i = 1; i < n; i++) {
+    let temp = arr[i]
+    let j = i - 1
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j]
+      j--
+    }
+    arr[j + 1] = temp
+  }
+  return arr
+}
