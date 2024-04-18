6c41b0715d4673a0ea181946668e0f7c39cdeeef
nechapay <nechapay@list.ru>
2024-04-18 13:27:32 +0300

    new tasks

--- js/leet-code.js
+++ js/leet-code.js
@@ -634,3 +634,41 @@ function removeNthFromEnd(head, n) {
 
   return head
 }
+
+function isValid(s) {
+  const p = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  }
+
+  const stk = []
+  for (const item of s) {
+    if (!stk && !p[item]) return false
+    if (p[item]) stk.push(item)
+    else if (item !== p[stk[stk.length - 1]]) return false
+    else stk.pop()
+  }
+
+  return stk.length === 0
+}
+
+function mergeTwoLists(list1, list2) {
+  let node =  new ListNode()
+  let head = node
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      node.next = list1
+      list1 = list1.next
+    } else {
+      node.next = list2
+      list2 = list2.next
+    }
+    node = node.next
+  }
+
+  node.next = list1 || list2
+
+  return head.next
+}
--- js/main.js
+++ js/main.js
@@ -267,7 +267,9 @@ function start() {
   //   [-2, 0, 0, 2],
   //   [-1, 0, 0, 1]
   // ])
-  console.log(linkedListToArray(removeNthFromEnd(arrayToLinkedList([1, 2, 3, 4, 5]), 2)))
+  // console.log(linkedListToArray(removeNthFromEnd(arrayToLinkedList([1, 2, 3, 4, 5]), 2)))
+  // console.log(isValid('()'))
+  console.log(linkedListToArray(mergeTwoLists(arrayToLinkedList([1, 2, 4]), arrayToLinkedList([1, 3, 4]))))
 }
 // closure trick let 1 2 3 var 3 3 3
 const f = () => {
